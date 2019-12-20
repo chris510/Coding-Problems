9f7a48a908b049da671b18e94964ce2846da9cb9
Chris Trinh <christrinh5@gmail.com>
2019-12-20 09:05:36 -0800

    Coin Change full annotations

--- leetcode/medium/322_coin_change_2.js
+++ leetcode/medium/322_coin_change_2.js
@@ -7,12 +7,15 @@ const coinChange = (coins, amount) => {
   table[0] = 0;
 
   coins.forEach((val) => {
+      // itterate through the full table; amt will be the sub problems
       for (let amt = 0; amt < table.length; amt++) {
           // finding how many coins of a current value can fit into the amount
           for (let qty = 0; qty * val <= amt; qty++) {
-              //
+              //sub problem to find the amount left after you take the certain coins
               remainder = amt - qty * val;
+              // look in our table for the remainder of coins + the quantity to find the minimum 
               attempt = table[remainder] + qty;
+              //if the attempt is better meaning less number of coins, we will replace the table amount.
               if (attempt < table[amt]) table[amt] = attempt;
           }
       }
