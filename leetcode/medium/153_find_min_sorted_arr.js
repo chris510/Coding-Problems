c22d59a8de69cc8f4e9d5847f7af08b5fe5351c9
Chris Trinh <christrinh5@gmail.com>
2020-01-09 18:23:23 -0800

    find minimum in sorted array

---
+++ leetcode/medium/153_find_min_sorted_arr.js
@@ -0,0 +1,36 @@
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+// Find the minimum element.
+
+// You may assume no duplicate exists in the array.
+
+// Thoughtprocess: We will be using binary search to find the minimum value of log n time and constant space. We will use left and right boundaries to look for the constant mid point and check if its greater than or less than the outer boundaries. If the midpoint is less than the right, we know the minimum value is on the left side since the right side is already sorted.
+
+var findMin = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2)
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return nums[left];
+};
+
+// Time Complexity: O(log n) we are using binary search to look through half the input array at each itteration.
+// Space Complexity: O(1);
+
+// Example 1:
+
+// Input: [3,4,5,1,2] 
+// Output: 1
+// Example 2:
+
+// Input: [4,5,6,7,0,1,2]
+// Output: 0
