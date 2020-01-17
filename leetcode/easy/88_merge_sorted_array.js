d2646c8c32dc22216b2c94b136143a290ffadcd7
Chris Trinh <christrinh5@gmail.com>
2020-01-16 21:09:35 -0800

    Merge sorted array optimized time

--- leetcode/easy/88_merge_sorted_array.js
+++ leetcode/easy/88_merge_sorted_array.js
@@ -8,25 +8,53 @@
 
 //Thought process: We can use two pointers to itterate through both arrays. We look at each point and then use the method splice depending on which number is greater. If the nums1 array is larger, then we can just move forward.
 
-const merge = (nums1, m, nums2, n) => {
-  nums1.splice(m, nums1.length);
-  nums2.splice(n, nums2.length);
+// const merge = (nums1, m, nums2, n) => {
+//   nums1.splice(m, nums1.length);
+//   nums2.splice(n, nums2.length);
 
-  let a = 0;
-  let b = 0;
+//   let a = 0;
+//   let b = 0;
 
-  while (b < n) {
-    // If the second array number is bigger or we run out of the first array to itterate through.
-    if (nums2[b] < nums1[a] || nums1[a] === undefined) {
-      nums1.splice(a, 0, nums2[b]);
-      a++;
-      b++;
-    } else { // first number in the array is bigger, so we will continue with the next comparison.
-      a++;
-    }
-  }
-  return nums1;
-};
+//   while (b < n) {
+//     // If the second array number is bigger or we run out of the first array to itterate through.
+//     if (nums2[b] < nums1[a] || nums1[a] === undefined) {
+//       nums1.splice(a, 0, nums2[b]);
+//       a++;
+//       b++;
+//     } else { // first number in the array is bigger, so we will continue with the next comparison.
+//       a++;
+//     }
+//   }
+//   return nums1;
+// };
+
+// When yous splice in a loop, it is n^2 because splicing has to reorganize the array
+
+// var merge = function(nums1, m, nums2, n) {
+//   let idx = nums1.length - 1;
+//   m--;
+//   n--;
+  
+//   while (idx >= 0) {
+//     if (m < 0) {
+//       nums1[idx] = nums2[n];
+//       n--;
+//     } else if (n < 0) {
+//       nums1[idx] = nums1[m];
+//       m--;
+//     } else {
+//       if (nums1[m] > nums2[n]) {
+//         nums1[idx] = nums1[m];
+//         m--;
+//       } else {
+//         nums1[idx] = nums2[n];
+//         n--;
+//       }
+//     }
+//     idx--;
+//   }
+
+// };
 
 // Time Complexity: O(n * k) where n and k are the lengths of both arrays.
 // Space Complexity: O(1) where space is constant since we are just modifying the original array.
