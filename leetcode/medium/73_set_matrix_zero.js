1ed5bff32f09019b9294e9ae6f9f780af18dd6eb
Chris Trinh <christrinh5@gmail.com>
2020-01-23 14:56:09 -0800

    Set zeroes with constant space

--- leetcode/medium/73_set_matrix_zero.js
+++ leetcode/medium/73_set_matrix_zero.js
@@ -31,6 +31,60 @@ var setZeroes = function(matrix) {
 
 // Can we do this in constant space?
 
+var setZeroes = function(matrix) {
+  let rowHasZero = false;
+  let colHasZero = false;
+  let rowLen = matrix.length;
+  let colLen = matrix[0].length
+  for (let i = 0; i < rowLen; i++) {
+    if (!matrix[i][0]) {
+      rowHasZero = true;
+      break;
+    }
+  }
+  // check col for 0
+  for (let j = 0; j < colLen; j++) {
+    if (!matrix[0][j]) {
+      colHasZero = true;
+      break;
+    }
+  }
+  // sets first row and col for 0.
+  for (let i = 0; i <rowLen; i++) {
+    for (let j = 0; j < colLen; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      } 
+    }
+  }
+  // go through and check the rows and cols for 0
+  for (let i=1; i < rowLen; i++) {
+    for (let j=1 ;j < colLen ;j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  // change the first row if theres a 0 in it
+  if (rowHasZero) {
+    for (let i = 0; i < rowLen; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+  //change the first col if theres a 0 init.
+  if (colHasZero) {
+    for (let j = 0; j < colLen; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+  return matrix;
+};
+
+// Time Complexity: O(n * m) where n and are the lengths of the main and sub array respectively;
+// Space Complexity: O(1) since we are using the first row and column as the 'true' and 'false' to indicate which ones we need to switch 0's. We don't use extrra space
+
+
 // Example 1:
 
 // Input: 
@@ -59,3 +113,44 @@ var setZeroes = function(matrix) {
 //   [0,4,5,0],
 //   [0,3,1,0]
 // ]
+
+
+// Use the first row and first column as a temporary marker
+// Use matrix[0][0] as a marker to see if the first row is zero
+// Use colZero as a marker to see if the first column is zero
+// var setZeroes = function(matrix) {
+//     let colZero = false;
+//     for (let i=0;i<matrix.length;i++) {
+
+//         if (matrix[i][0]==0) {
+//             colZero = true;
+//         }
+
+//         for (let j=1;j<matrix[0].length;j++) {
+//             if (matrix[i][j]==0) {
+//                 matrix[i][0] = 0;
+//                 matrix[0][j] = 0;
+//             }
+//         }
+//     }
+
+//     for (let i=1;i<matrix.length;i++) {
+//         for (let j=1;j<matrix[0].length;j++) {
+//             if (matrix[i][0]==0 || matrix[0][j]==0) {
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+
+//     if (colZero) {
+//         for (let i=0;i<matrix.length;i++) {
+//             matrix[i][0] = 0;
+//         }
+//     }
+
+//     if (matrix[0][0]==0) {
+//         for (let i=0;i<matrix[0].length;i++) {
+//             matrix[0][i] = 0;
+//         }
+//     }
+// }
