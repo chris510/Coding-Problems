824068d4630d14ad82f186947720b998653bde7f
Chris Trinh <christrinh5@gmail.com>
2020-01-29 12:30:46 -0800

    Clone graph annotations

--- leetcode/medium/49_group_anagrams.js
+++ leetcode/medium/49_group_anagrams.js
@@ -6,13 +6,13 @@ var groupAnagrams = function(strs) {
   let anagrams = {};
   
   for (let word of strs) {
-      let sortedWord = word.split('').sort().join();
-      
-      if (sortedWord in anagrams) {
-          anagrams[sortedWord].push(word);
-      } else {
-          anagrams[sortedWord] = [word];
-      }
+    let sortedWord = word.split('').sort().join();
+    
+    if (sortedWord in anagrams) {
+        anagrams[sortedWord].push(word);
+    } else {
+        anagrams[sortedWord] = [word];
+    }
   }
   
   return Object.values(anagrams);
