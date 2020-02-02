3553b4b0963426178741c4b0433149b4bf5548e0
Chris Trinh <christrinh5@gmail.com>
2020-02-02 13:05:34 -0800

    Word search annotations

--- leetcode/medium/79_word_search.js
+++ leetcode/medium/79_word_search.js
@@ -2,6 +2,38 @@
 
 // The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
+// Traverse the matrix using depth first search to find the corresponding letters while keeping a count variable to see what character we are on. If we reach the end of the word legnth then we know we reache dthe end, otherwise its false and we don't have to check the letter cause it doens't match. In order for us to traverse, we need to match the three conditions.
+
+var exist = function(board, word) {
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[row].length; col++) {
+      if (board[row][col] === word[0] && check(row, col, 0, board, word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const check = (row, col, count, board, word) => {
+  if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
+  if (board[row][col] != word[count]) return false;
+  if (count == word.length - 1 ) return true;
+  
+  let temp = board[row][col];
+  board[row][col] = ' ';
+  let found = check(row + 1, col, count + 1, board, word)
+           || check(row - 1, col, count + 1, board, word)
+           || check(row, col + 1, count + 1, board, word)
+           || check(row, col - 1, count + 1, board, word);
+  
+  board[row][col] = temp;
+  return found 
+}
+
+// Time Complexity: O(n * m) where n and m are the rows an cols or O(n) where n is the number of cells on the board on the worst case
+// Space Complexity: O(n * m) we are modifying board in place but the stack of recursive calls is the entire board
+
 // Example:
 
 // board =
