50b6616fcdf7de36d9ccd88f9eed10acfb0934f4
Chris Trinh <christrinh5@gmail.com>
2020-03-30 22:55:27 -0700

    Add two numbers

---
+++ python/linked_lists/2_addtwonumbers.py
@@ -0,0 +1,39 @@
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Example:
+
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+
+def addTwoNumbers(self, l1: ListNode, l2: ListNode):
+  dummyHead = ListNode(0)
+  l3 = dummyHead
+  sum = 0
+  carry = 0
+  
+  while l1 or l2 or sum:
+    if l1:
+      sum += l1.val
+      l1 = l1.next
+    
+    if l2:
+      sum += l2.val
+      l2 = l2.next
+    
+    if sum >= 10:
+      sum -= 10
+      carry = 1
+      
+    l3.next = ListNode(sum)
+    l3 = l3.next
+    sum = carry
+    carry = 0
+
+  return dummyHead.next
+
+# Time: O(n * m) where n and m are the lengths of both linked lists
+# Space: O(n * m) where we are creating a new linkedlist with the length of the list att most the length of both linked lists
+        
--- python/trees/98_validatebinarysearchtree.py
+++ python/trees/98_validatebinarysearchtree.py
@@ -27,6 +27,7 @@
 # Output: false
 # Explanation: The root node's value is 5 but its right child's value is 4.
 
+# Recursion
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
       return self.validateBST(root, float(-inf), float(inf))
@@ -42,3 +43,55 @@ class Solution:
       return leftisValid and rightisValid
     
 # Time: O(n) | Space: O(n)
+
+# Itterative 
+# class Solution:
+#   def isValidBST(self, root: TreeNode) -> bool:
+#       if not root:
+#         return True
+      
+#       stack = [(root, float(-inf), float(inf))]
+#       while stack:
+#         node, min_val, max_val = stack.pop()
+#         if not node:
+#           continue
+#         if node.val <= min_val or node.val >= max_val:
+#           return False
+#         stack.append((node.right, node.val, max_val))
+#         stack.append((node.left, min_val, node.val))
+#       return True
+
+# Time: O(n) | Space: O(n)
+
+# def isValidBST(self, root: TreeNode) -> bool:
+#   stack = []
+#   inorder = float(-inf)
+  
+#   while stack or root:
+#     while root:
+#       stack.append(root)
+#       root = root.left
+#     node = stack.pop()
+#     if node.val <= inorder:
+#       return False
+#     inorder = node.val
+#     root = node.right
+#   return True
+
+# Time: O(n) | Space: O(n)
+
+# inorder traversal
+def isValidBST(self, root: TreeNode) -> bool:
+  stack = []
+  inorder = float(-inf)
+  
+  while stack or root:
+    while root:
+      stack.append(root)
+      root = root.left
+    node = stack.pop()
+    if node.val <= inorder:
+      return False
+    inorder = node.val
+    root = node.right
+  return True
