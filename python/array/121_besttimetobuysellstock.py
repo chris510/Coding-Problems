1c37c6eea1e9865e99572dc039bae7936f4b8e99
Chris Trinh <christrinh5@gmail.com>
2020-04-06 13:54:16 -0700

    Best time to buy and sell stock 2

--- python/array/121_besttimetobuysellstock.py
+++ python/array/121_besttimetobuysellstock.py
@@ -16,17 +16,16 @@
 # Output: 0
 # Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        if (len(prices)) < 2: return max_profit
-        min_num = prices[0]
-        
-        for num in prices[1:]:
-          min_num = min(num, min_num)
-          max_profit = max(max_profit, num - min_num)
-        
-        return max_profit
+def maxProfit(self, prices: List[int]) -> int:
+  max_profit = 0
+  if (len(prices)) < 2: return max_profit
+  min_num = prices[0]
+  
+  for num in prices[1:]:
+    min_num = min(num, min_num)
+    max_profit = max(max_profit, num - min_num)
+  
+  return max_profit
         
 # Time: O(N)
 # Space: O(1)
