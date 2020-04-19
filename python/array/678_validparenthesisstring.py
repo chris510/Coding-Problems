b669156e03e696f818a7e15dfe7007ff261ba8f0
Chris Trinh <christrinh5@gmail.com>
2020-04-18 18:53:45 -0700

    Min path sum python

--- python/array/678_validparenthesisstring.py
+++ python/array/678_validparenthesisstring.py
@@ -44,5 +44,29 @@ def checkValidString(s):
   return True
 
 # Time: O(n) | Space: O(n)
-          
+
+def checkValidString(s):
+  if len(s) < 1: return True
+  
+  balance = 0
+  for char in s:
+    if char == ")":
+      balance -= 1
+    else:
+      balance += 1
+    if balance < 0:
+      return False
+  if balance == 0: return True
+  balance = 0
+  for char in reversed(s):
+    if char == "(":
+      balance -= 1
+    else:
+      balance += 1
+    if balance < 0:
+      return False
+  return True
+
+# Time: O(n) | Space: O(1)
+        
         
