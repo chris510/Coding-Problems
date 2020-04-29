8542383cb7f50109b9a5658452cdff342d7ee87c
Chris Trinh <christrinh5@gmail.com>
2020-04-29 14:32:22 -0700

    Add unique numbers

--- python/dp/221_maximalsquare.py
+++ python/dp/221_maximalsquare.py
@@ -41,3 +41,23 @@ def maximalSquare(self, matrix: List[List[str]]) -> int:
   return max_square * max_square
 
 # Time: O(n * m) | Space: O(n)
+
+def maximalSquare(self, matrix: List[List[str]]) -> int:
+  row = len(matrix)
+  if row == 0: return 0
+  col = len(matrix[0])
+  if col == 0: return 0
+  
+  dp = [[0 for i in range(col + 1)] for j in range(row + 1)]
+  max_square = 0
+  
+  for i in range(1, row + 1):
+    for j in range(1, col + 1):
+      if matrix[i - 1][j - 1] == "1":
+        top_left = dp[i - 1][j-1]
+        left = dp[i - 1][j]
+        top = dp[i][j - 1]
+        dp[i][j] = min(top_left, left, top) + 1
+        max_square = max(max_square, dp[i][j])
+  return max_square * max_square
+              
